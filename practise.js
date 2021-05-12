2aec03b2dbc5035b902f6ff64931a87943d492df
mdafsha9 <mdafsha9@gmail.com>
2021-05-12 06:08:29 -0500

    JavaScript can change HTML attribute values

--- practise.js
+++ practise.js
@@ -1,3 +1,10 @@
 function myCont(){
   document.getElementById('cont').innerHTML = "Hello JavaScript World!";
 }
+
+function openLock(){
+  document.getElementById('myImg').src = "assets/lockopen.png";
+}
+function closeLock(){
+  document.getElementById('myImg').src="assets/lockclo.png";
+}
