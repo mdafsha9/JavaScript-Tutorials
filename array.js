892e7c13121099915dbb9b81c2f8b55050410e49
mdafsha9 <mdafsha9@gmail.com>
2021-10-02 00:19:41 -0500

    using new keyword to create an array

---
+++ array.js
@@ -0,0 +1,14 @@
+//Create an array
+const myArr = ["Apple", "Mango", "Banana", "Orange"];
+document.getElementById("arr").innerHTML = myArr;
+
+//First create an array and then add elements
+const myArrEle = [];
+myArrEle[0] = "Volvo";
+myArrEle[1] = "Accord";
+myArrEle[2] = "BMW";
+document.getElementById("addEle").innerHTML = myArrEle;
+
+//Using javascript keyword new
+const cars = new Array("Safari", "Honda", "Chervolt");
+document.getElementById("key").innerHTML = cars;
--- this.js
+++
@@ -1,2 +0,0 @@
-const myArr = ["Apple", "Mango", "Banana", "Orange"];
-document.getElementById("arr").innerHTML = myArr;
