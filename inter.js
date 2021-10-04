e9cbaddef4b305c17734a8efc3e92eef8c31673f
mdafsha9 <mdafsha9@gmail.com>
2021-10-04 05:17:50 -0500

    isNaN() function

---
+++ inter.js
@@ -0,0 +1,13 @@
+function myFunction(){
+  var res = " ";
+res = res + Number.isNaN(123) + "<br>";
+res = res + Number.isNaN("123") + "<br>";
+res = res + Number.isNaN(5-2) + "<br>";
+res = res + Number.isNaN(0) + "<br>";
+res = res + Number.isNaN("Hello") + "<br>";
+res = res + Number.isNaN(0/0) + "<br>";
+res = res + Number.isNaN(NaN) + "<br>";
+res = res + Number.isNaN(undefined) + "<br>";
+
+  document.getElementById("nan").innerHTML = res;
+}
