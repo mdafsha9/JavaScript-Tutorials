87362dcbacbd5b526dee3cb38ceffe7f9fcf6cce
mdafsha9 <mdafsha9@gmail.com>
2021-10-08 03:14:46 -0500

    function() constructor

--- function.js
+++ function.js
@@ -1,4 +1,4 @@
-//function declartions
+//function declarations
 document.getElementById("f1").innerHTML = myFunction(3,4);
 function myFunction(a,b){
   return a+b;
@@ -9,3 +9,7 @@ const x = function(c,d){
   return c*d;
 };
 document.getElementById("f2").innerHTML = x(2,5);
+
+//function() constructor
+const myFunction1 = new Function("e", "f", "return e*f");
+document.getElementById("f3").innerHTML = myFunction1(8,2);
