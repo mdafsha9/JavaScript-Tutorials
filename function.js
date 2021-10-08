322b03324e8ea92901e67660c99ab77c32eff586
mdafsha9 <mdafsha9@gmail.com>
2021-10-08 03:04:53 -0500

    anonymous function/function expression

--- function.js
+++ function.js
@@ -1,5 +1,11 @@
+//function declartions
 document.getElementById("f1").innerHTML = myFunction(3,4);
-
 function myFunction(a,b){
   return a+b;
 }
+
+//function expressions
+const x = function(c,d){
+  return c*d;
+};
+document.getElementById("f2").innerHTML = x(2,5);
