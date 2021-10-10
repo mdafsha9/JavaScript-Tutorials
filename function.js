d1db0461b46707aade96302f6fcbd3fb7d7e4629
mdafsha9 <mdafsha9@gmail.com>
2021-10-09 20:27:13 -0500

    example for self-invoking function

--- function.js
+++ function.js
@@ -13,3 +13,8 @@ document.getElementById("f2").innerHTML = x(2,5);
 //function() constructor
 const myFunction1 = new Function("e", "f", "return e*f");
 document.getElementById("f3").innerHTML = myFunction1(8,2);
+
+//self-invoking functions
+(function(){
+  document.getElementById("f4").innerHTML = "Hello Javascript world!";
+})();
