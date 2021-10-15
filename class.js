1b14c6f114a1ec092d3819865cbe6e0c88fb59fa
mdafsha9 <mdafsha9@gmail.com>
2021-10-14 22:22:45 -0500

    class inheritance example

--- class.js
+++ class.js
@@ -27,3 +27,24 @@ class Car{
 const myCar = new Car("Ford", 2014);
 document.getElementById("c2").innerHTML = myCar.age();
 document.getElementById("c3").innerHTML = myCar.current();
+
+//class inheritance
+class Car1{
+  constructor(brand){
+    this.carName = brand;
+  }
+  present(){
+    return "I have a " +this.carName;
+  }
+}
+class Model extends Car1{
+  constructor(brand, mod){
+    super(brand);
+    this.model = mod;
+  }
+  show(){
+    return this.present() + ", it is a " + this.model;
+  }
+}
+const myCar1 = new Model("Ford", "Mustang");
+document.getElementById("c4").innerHTML = myCar1.show();
