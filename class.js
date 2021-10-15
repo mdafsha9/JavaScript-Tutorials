73f040e9dde43f6f9223fc98fad6971f6e477fc3
mdafsha9 <mdafsha9@gmail.com>
2021-10-14 21:37:09 -0500

    constructor methods

--- class.js
+++ class.js
@@ -1,9 +1,29 @@
-class Car{
+//javascript class
+class Name{
   constructor(first, last){
     this.first = first;
     this.last = last;
   }
 }
-  const myCar1 = new Car("John", "Doe");
-  const myCar2 = new Car("Marie", "Leo");
-  document.getElementById("c1").innerHTML = myCar1.first + " " +myCar2.last;
+  const myName1 = new Name("John", "Doe");
+  const myName2 = new Name("Marie", "Leo");
+  document.getElementById("c1").innerHTML = myName1.first + " " +myName2.last;
+
+//javascript constructor methods
+class Car{
+  constructor(carName, year){
+    this.carName = carName;
+    this.year = year;
+  }
+  age(){
+    let date = new Date();
+    return date.getFullYear()-this.year;
+  }
+  current(){
+    let date = new Date();
+    return date;
+  }
+}
+const myCar = new Car("Ford", 2014);
+document.getElementById("c2").innerHTML = myCar.age();
+document.getElementById("c3").innerHTML = myCar.current();
