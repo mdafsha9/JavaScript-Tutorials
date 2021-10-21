866f38e63a7654d29550192ad489992d64cabbc1
mdafsha9 <mdafsha9@gmail.com>
2021-10-21 00:18:56 -0500

    create a date with specified date and time

--- date.js
+++ date.js
@@ -1,3 +1,7 @@
 //javascript default Date
 const d = new Date();
 document.getElementById("d1").innerHTML = d;
+
+//create an object using new Date() with a specified date & setTimeout(function () {
+  const d1 = new Date(2021, 10, 21, 10, 46, 30, 02);
+  document.getElementById("d2").innerHTML = d;
