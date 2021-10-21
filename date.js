17a847c45401e7cf3c5e7406b7ef1b527c4cc4d7
mdafsha9 <mdafsha9@gmail.com>
2021-10-21 00:24:28 -0500

    toString()

--- date.js
+++ date.js
@@ -3,5 +3,9 @@ const d = new Date();
 document.getElementById("d1").innerHTML = d;
 
 //create an object using new Date() with a specified date & setTimeout(function () {
-  const d1 = new Date(2021, 10, 21, 10, 46, 30, 02);
-  document.getElementById("d2").innerHTML = d;
+  const d1 = new Date(2021, 9, 21, 10, 46, 30, 02);
+  document.getElementById("d2").innerHTML = d1;
+
+//Date methods--toString();
+const d2 = new Date();
+document.getElementById("d3").innerHTML = d2.toString();
