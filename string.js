253e28595a22f1821b15a2d7a39891178628075d
mdafsha9 <mdafsha9@gmail.com>
2021-10-21 20:57:58 -0500

    git add .

--- string.js
+++ string.js
@@ -1,2 +1,7 @@
 let firstName = "Afsha";
 document.getElementById("demo").innerHTML = firstName;
+
+//Backslash
+let lastName = "Doe";
+document.getElementById("s1").innerHTML = "'Hey!' this is a Doe";
+document.getElementById("s2").innerHTML = "\"Hey!\" this is a Doe";
