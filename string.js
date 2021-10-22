44a3b339623b64e7d73e2c93f8c4c55e63639b8e
mdafsha9 <mdafsha9@gmail.com>
2021-10-21 21:02:49 -0500

    string length

--- string.js
+++ string.js
@@ -5,3 +5,8 @@ document.getElementById("demo").innerHTML = firstName;
 let lastName = "Doe";
 document.getElementById("s1").innerHTML = "'Hey!' this is a Doe";
 document.getElementById("s2").innerHTML = "\"Hey!\" this is a Doe";
+//line break
+document.getElementById("s3").innerHTML = "Hey! this is a \n" + firstName + " " + lastName;
+
+//string length
+document.getElementById("s4").innerHTML = firstName.length;
