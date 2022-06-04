139c833222aaeecec1cf3fcbffefed9d053d65d8
mdafsha9 <mdafsha9@gmail.com>
2022-06-04 02:37:22 -0500

    primitive data types except boolean

--- prac.js
+++ prac.js
@@ -2,6 +2,17 @@
 //1. string
 
 let firstName = "Cathy";
+let age = 22;
+let hobby;
+let tree = null;
 
+let num1 = BigInt(12);
+let num2 = 123n;
 console.log(firstName);
 console.log(typeof firstName);
+console.log(age, typeof age);
+console.log(hobby);
+console.log(tree, typeof tree);
+
+console.log(num1+num2);
+console.log(Number.MAX_SAFE_INTEGER);
