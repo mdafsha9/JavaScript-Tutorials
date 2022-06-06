df9713add27828f011e27bb938ead3938bd44f73
mdafsha9 <mdafsha9@gmail.com>
2022-06-05 21:22:04 -0500

    Boolean data type

--- prac.js
+++ prac.js
@@ -1,28 +1,6 @@
-//primitive datatypes
-//1. string
+//boolean and camparison operator
 
-let firstName = "Cathy";
-let age = 22;
-let hobby;
-let tree = null;
+let num1 = 7;
+let num2 = 5;
 
-let num1 = BigInt(12);
-let num2 = 123n;
-console.log(firstName);
-console.log(typeof firstName);
-console.log(age, typeof age);
-console.log(hobby);
-console.log(tree, typeof tree);
-
-console.log(num1+num2);
-console.log(Number.MAX_SAFE_INTEGER);
-
-//converting numb to string
-age = age + " "; or
-console.log(age, typeof age);
-
-
-
-//converting string to number
-firstName = +"22";
-console.log(firstName, typeof firstName);
+console.log(num1>num2);
