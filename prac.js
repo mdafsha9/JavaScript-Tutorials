ba792f46dfb819b62000fa55983b456612edb9f2
mdafsha9 <mdafsha9@gmail.com>
2022-06-05 21:24:09 -0500

    comparison operator

--- prac.js
+++ prac.js
@@ -1,6 +1,10 @@
 //boolean and camparison operator
 
-let num1 = 7;
-let num2 = 5;
+let num1 = "7";
+let num2 = 7;
 
-console.log(num1>num2);
+console.log(num1>=num2);
+
+
+console.log(num1 == num2);
+console.log(num1 === num2);
