6513698acf0f534995187569e505ea14186dbe63
mdafsha9 <mdafsha9@gmail.com>
2022-06-15 01:32:07 -0500

    and operator

--- prac.js
+++ prac.js
@@ -1,6 +1,10 @@
-//ternary operator
+//And or oprator
 
-let age = 3;
+let name = "Lorie";
+let age = 18;
 
-let drink = age > 5 ? "Coffee" : "milk";
-console.log(drink);
+if(name[0] === "L" && age>=18){
+  console.log("Name starts with L and age is 18");
+}else{
+  console.log("both condition must true!");
+}
