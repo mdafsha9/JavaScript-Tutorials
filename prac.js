fb58f22da165b6db699051d1b903291a9acadc77
mdafsha9 <mdafsha9@gmail.com>
2022-06-15 00:51:02 -0500

    num to str conv and str to num conv

--- prac.js
+++ prac.js
@@ -1,7 +1,13 @@
-//typeof
+//number to string conversion and string to number conversion
+//number to string conversion
+let number = 22;
 
-let digit = 22;
-let alpha = "22";
+console.log(number, typeof number);
 
-console.log(digit == alpha);
-console.log(digit === alpha);
+number = 22 + " ";
+console.log(number, typeof number);
+
+let name = "Royal";
+console.log(name, typeof name);
+name = + "Royal";
+console.log(name, typeof name);
