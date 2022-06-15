d3b72ed6825cf141fde4d1d725b9085014099941
mdafsha9 <mdafsha9@gmail.com>
2022-06-15 01:33:45 -0500

    OR operator

--- prac.js
+++ prac.js
@@ -1,10 +1,10 @@
 //And or oprator
 
 let name = "Lorie";
-let age = 18;
+let age = 15;
 
-if(name[0] === "L" && age>=18){
+if(name[0] === "L" || age>=18){
   console.log("Name starts with L and age is 18");
 }else{
-  console.log("both condition must true!");
+  console.log("Atleast one condition must true!");
 }
