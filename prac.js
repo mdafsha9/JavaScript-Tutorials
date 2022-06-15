e8f4decc7ce0a5802c623e700866c7f2a4dd81c0
mdafsha9 <mdafsha9@gmail.com>
2022-06-15 00:27:11 -0500

    string indexing

--- prac.js
+++ prac.js
@@ -1,30 +1,7 @@
-//switch statement
+//String indexing
 
-let day = 3;
+let fruits = "Apple";
 
-switch(day){
-  case 0:
-    console.log("Sunday");
-    break;
-  case 1:
-    console.log("Monday");
-    break;
-  case 2:
-    console.log("Tuesday");
-    break;
-  case 3:
-    console.log("Wednesday");
-    break;
-  case 4:
-    console.log("Thursday");
-    break;
-  case 5:
-    console.log("Friday");
-    break;
-  case 6:
-    console.log("Saturday");
-    break;
-  default:
-    console.log("Invalid!");
-    break;
-}
+console.log(fruits);
+console.log(fruits.length);
+console.log(fruits[fruits.length-1]);
