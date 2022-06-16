67938741ae25c1b58a760cefb07be2a0b24ee06b
mdafsha9 <mdafsha9@gmail.com>
2022-06-16 00:35:04 -0500

    while loop

--- prac.js
+++ prac.js
@@ -1,27 +1,8 @@
-//switch statement
-let day = 8;
-switch(day){
-  case 0:
-    console.log("Sunday!");
-    break;
-  case 1:
-    console.log("Monday!");
-    break;
-  case 2:
-    console.log("Tuesday!");
-    break;
-  case 3:
-    console.log("Wednesday!");
-    break;
-  case 4:
-    console.log("Thursday!");
-    break;
-  case 5:
-    console.log("Friday!");
-    break;
-  case 6:
-    console.log("Saturday!");
-    break;
-  default:
-  console.log("Invalid");
+//while loop
+
+let i = 0;
+
+while(i<=10){
+  console.log(i);
+  i++;
 }
