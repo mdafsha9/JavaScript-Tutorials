a42f5687ee6361d93987656fd51d2fb52ad37dc9
mdafsha9 <mdafsha9@gmail.com>
2022-06-17 01:11:44 -0500

    arrays

--- prac.js
+++ prac.js
@@ -1,8 +1,10 @@
-//do while loop
+//arrays
 
-let i = 1;
-
-do{
-  console.log(i);
-  i++;
-}while(i<=0)
+const fruits = ["Apple", "Mango", "Orange"];
+console.log(fruits);
+console.log(typeof fruits);
+console.log(Array.isArray(fruits));
+fruits[1] = "Banana";
+console.log(fruits);
+const mixed = [1,2,3.5, "Cantaloup", null, undefined, ["Pear", "Dove"], {firstName : 'Lories', lastName:'Wright'}];
+console.log(mixed);
