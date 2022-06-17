233f1493bc43bb317bbf82d4ba5a557dd3d7ff7c
mdafsha9 <mdafsha9@gmail.com>
2022-06-17 01:53:49 -0500

    slice() method

--- prac.js
+++ prac.js
@@ -1,6 +1,12 @@
 //clone array
 let array1 = ["item1", "item2"];
-let array2 = array1;
-console.log(array1 === array2);
+// let array2 = array1;
+array1.push("item3");
+// console.log(array1 === array2);
+
+
+//we can clone array1 elements in array2 but we cannot copy array1 push elements
+
+let array2 = array1.slice(0);
 console.log(array1);
 console.log(array2);
