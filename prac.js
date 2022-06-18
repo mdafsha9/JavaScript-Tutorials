fe7f491842580922f513f761b00f65dcc0592cb7
mdafsha9 <mdafsha9@gmail.com>
2022-06-18 01:48:14 -0500

    complet array except array destructuring

--- prac.js
+++ prac.js
@@ -34,3 +34,26 @@ let array1 = ["item1", "item2", "item3"];
 let array2 = [...array1, "item4"];
 console.log(array1);
 console.log(array2);
+
+//for loop
+
+for(let i=0; i<fruits.length; i++){
+  console.log(fruits[i]);
+}
+
+//while loop
+let i =0;
+while(i<fruits.length){
+  console.log(fruits[i]);
+  i++;
+}
+
+//for of loop
+for(let fruit of fruits){
+  console.log(fruit);
+}
+
+//for in loop
+for(let index in fruits){
+  console.log(fruits[index]);
+}
