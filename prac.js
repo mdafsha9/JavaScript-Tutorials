05e18883dc49fd21d8c7c04c2bd197cbb77e2bf9
mdafsha9 <mdafsha9@gmail.com>
2022-06-21 05:55:35 -0500

    create an object using new Object()

--- prac.js
+++ prac.js
@@ -12,3 +12,14 @@ const person = {
 
 console.log("My name is " + person.firstName+ " " + person.lastName + " and i'm " +person.age+ " years old.");
 console.log("My name is " +person.fullName());
+
+//create a new object using new Object()
+
+const employee = new Object();
+employee.firstName = "Donna";
+employee.lastName = "Erric";
+employee.age = 30;
+employee.fullName = function(){
+  return this.firstName + " " + this.lastName;
+}
+console.log("My name is " +employee.fullName());
