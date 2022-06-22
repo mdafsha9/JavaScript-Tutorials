3816e075faa87e7f9b7669bcfd3d855f2014a62f
mdafsha9 <mdafsha9@gmail.com>
2022-06-22 01:29:35 -0500

    objects

--- prac.js
+++ prac.js
@@ -2,24 +2,9 @@
 
 const person = {
   firstName : "Lorie",
-  lastName : "Wright",
-  age : 25,
-  eyecolor : "Brown",
-  fullName : function(){
-    return this.firstName + " " + this.lastName;
-  }
+  age : 56,
+  "person hobbies" : ["Playing", "dancing", "reading"]
 }
 
-console.log("My name is " + person.firstName+ " " + person.lastName + " and i'm " +person.age+ " years old.");
-console.log("My name is " +person.fullName());
-
-//create a new object using new Object()
-
-const employee = new Object();
-employee.firstName = "Donna";
-employee.lastName = "Erric";
-employee.age = 30;
-employee.fullName = function(){
-  return this.firstName + " " + this.lastName;
-}
-console.log("My name is " +employee.fullName());
+console.log(person, typeof person);
+console.log(person["person hobbies"]);
