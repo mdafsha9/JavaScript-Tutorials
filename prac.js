e7cb0ead60e2b9253a4dec6b347426b0b4b6849b
mdafsha9 <mdafsha9@gmail.com>
2022-06-28 06:42:08 -0500

    prototype

--- prac.js
+++ prac.js
@@ -10,8 +10,9 @@ function Person(first, last, age, eye) {
   }
 }
 
-
+Person.prototype.nationality = "Indian";
 const myFriend = new Person("Black", "Berry", 25, "brown");
 const myNeighbour = new Person("obama", "barak", 40, "blue");
 
 console.log("My name is " +myNeighbour.name() + " and i am " + myFriend.age) ;
+console.log("My nationality is " + myFriend.nationality);
