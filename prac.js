4e46bf1d00b3a5dd97b7b124a2e759693a68705a
mdafsha9 <mdafsha9@gmail.com>
2022-07-04 02:36:00 -0500

    function declaration

--- prac.js
+++ prac.js
@@ -1,33 +1,6 @@
-//JavaScript classes
-class Fruits{
-  constructor(name, quantity){
-    this.name = name;
-    this.quantity = quantity;
-  }
-  isHealthy(){
-    return `${this.name} is good for health!`;
-  }
-  isWater(){
-    return true;
-  }
+//function declarations
+
+function myFunc(a,b){
+  console.log(a+b);
 }
-
-const fruit1  = new Fruits("Apple", 6);
-console.log(fruit1);
-console.log(fruit1.isHealthy());
-console.log(fruit1.isWater());
-
-class Vegitables extends Fruits{
-
-    constructor(name, quantity, price){
-      super(name, quantity);
-      this.price = price;
-    }
-
-  isVeggi(){
-    return `${this.name} is a vegitable`;
-  }
-}
-const vegii1 = new Vegitables("Tomato", 12, 25);
-console.log(vegii1);
-console.log(vegii1.isVeggi());
+myFunc(2,5);
