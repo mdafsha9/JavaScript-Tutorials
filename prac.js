2dba4821edb408d4e3e76b784a03a9d345a077cb
mdafsha9 <mdafsha9@gmail.com>
2022-08-05 06:56:29 -0500

    slice() method

--- prac.js
+++ prac.js
@@ -14,3 +14,28 @@ console.log(firstName);
 //const keyword
 const pi = 1.36;
 console.log(pi*2);
+
+//JS strings
+
+let fruit = "Mango";
+for(i=0; i< fruit.length; i++){
+  console.log(fruit[i]);
+}
+
+//trim()
+fruit = "    ApPle    ";
+console.log(fruit.length);
+console.log(fruit);
+
+newFruit =  fruit.trim();
+console.log(newFruit);
+console.log(newFruit.length);
+
+//toUpperCase() method
+console.log(newFruit.toUpperCase());
+//toLowerCase() method
+console.log(newFruit.toLowerCase());
+
+//slice() method
+let newFruits = newFruit.slice(2);
+console.log(newFruits);
