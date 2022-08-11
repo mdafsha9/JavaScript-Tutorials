655ba8e6b9b8e6f567443d9fcebfe91e35e502a1
mdafsha9 <mdafsha9@gmail.com>
2022-08-11 06:30:35 -0500

    spread operator for array & objects

--- prac.js
+++ prac.js
@@ -1,45 +1,19 @@
-//arrays
-let person = {
-  firstName : "Lorie",
-  lastName : "Wright",
-  age : 22,
-  "person hobbies" : ["reading", "playing", "singing"]
+//spread operator in arrays and objects
+
+let array1 = ["item1", "item2" ];
+
+let array2 = ["item3", "item4"];
+
+const myArr = [...array1, ...array2, "item5"];
+console.log(myArr);
+
+let obj1 = {
+  key1 : "val1",
+  key2 : "val2"
 }
-console.log(person["person hobbies"]);
-
-let employee = {};
-employee.firstName = "Cathy";
-employee.lastName = "Krumm";
-employee.age = 25;
-employee.color = "white";
-
-console.log(employee);
-
-let student = new Object();
-student.firstName = "Naina";
-student.lastName = "Shestra";
-student.age = 35;
-student.color = "Black";
-console.log(student);
-
-for(let key in student){
-  console.log(`${key} : ${student[key]}`);
+let obj2 = {
+  key3 : "val3",
+  key4 : "val4"
 }
-
-const key1 = "objKey1";
-const key2 = "objKey2";
-
-const value1 = "myValue1";
-const value2 = "myValue2";
-
-const obj = {
-  key1 : value1,
-  key2 : value2
-}
-console.log(obj);
-
-const obj1 = {
-  [key1] : value1,
-  [key2] : value2
-}
-console.log(obj1);
+const myObj = {...obj1, ...obj2, key5:"val5"};
+console.log(myObj);
