f3de895f1ea2d79ca337f19189fcc4f7cfe882f1
mdafsha9 <mdafsha9@gmail.com>
2022-08-12 06:56:01 -0500

    constructor function and object prototype

--- prac.js
+++ prac.js
@@ -13,3 +13,24 @@ const myfriend2 = new Person("Haritha", "majiga", "Brown", 25);
 
 console.log("My friend eye color is " + " " + myfriend1.eye);
 console.log("My friend name is " + " " + myfriend2.firstName + " " +myfriend2.lastName);
+
+myfriend1.nationality = "English";
+
+console.log("My friend nationality is " + myfriend1.nationality);
+
+myfriend2.name = function(){
+  return this.firstName + " " +this.lastName;
+}
+
+console.log("My fullname is " + myfriend2.name());
+
+
+Person.prototype.hobbies = ["Reading", "Singing"];
+
+console.log("My friend hobbies are " + myfriend2.hobbies);
+
+Person.prototype.name = function(){
+  return this.firstName + " " + this.lastName;
+}
+
+console.log("My friend name is " + myfriend1.name());
