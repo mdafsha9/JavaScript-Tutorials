61851e1674f9b8df57c0908260ce49622e8af546
mdafsha9 <mdafsha9@gmail.com>
2022-08-12 07:28:24 -0500

    Started sets

--- prac.js
+++ prac.js
@@ -1,36 +1,3 @@
-//javascript object constructor function
-
-function Person(first, last, eye, age){
-  this.firstName = first;
-  this.lastName = last;
-  this.eye = eye;
-  this.age = age;
-}
-
-const myfriend1 = new Person("Harika", "Perugu", "Black", 22);
-const myfriend2 = new Person("Haritha", "majiga", "Brown", 25);
-
-
-console.log("My friend eye color is " + " " + myfriend1.eye);
-console.log("My friend name is " + " " + myfriend2.firstName + " " +myfriend2.lastName);
-
-myfriend1.nationality = "English";
-
-console.log("My friend nationality is " + myfriend1.nationality);
-
-myfriend2.name = function(){
-  return this.firstName + " " +this.lastName;
-}
-
-console.log("My fullname is " + myfriend2.name());
-
-
-Person.prototype.hobbies = ["Reading", "Singing"];
-
-console.log("My friend hobbies are " + myfriend2.hobbies);
-
-Person.prototype.name = function(){
-  return this.firstName + " " + this.lastName;
-}
-
-console.log("My friend name is " + myfriend1.name());
+//JavaScript object Set
+ const numbers = new Set([1,2,3,4,5,6]);
+ console.log(numbers);
