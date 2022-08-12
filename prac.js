e05b0cd8edd9b82f9e26e41731c0f5e830826956
mdafsha9 <mdafsha9@gmail.com>
2022-08-12 07:29:39 -0500

    add( ) method in Sets

--- prac.js
+++ prac.js
@@ -1,3 +1,8 @@
 //JavaScript object Set
- const numbers = new Set([1,2,3,4,5,6]);
+ const numbers = new Set();
+ numbers.add(1);
+ numbers.add(2);
+ numbers.add(3);
+ numbers.add(4);
+ numbers.add(5);
  console.log(numbers);
